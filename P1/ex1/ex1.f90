6fa1826afbf973b34d4b9cd5e9639f1eefd0f08a
HP_ARMONBAI\arnau <arnau.montull@gmail.com>
2026-09-18 11:33:50 +0200

    Refactor EX1 program to read an integer and a real number separately, calculate their sum, and write the result to a file.

--- P1/ex1/ex1.f90
+++ P1/ex1/ex1.f90
@@ -1,13 +1,21 @@
 PROGRAM EX1
     IMPLICIT NONE
-    INTEGER :: A, B
+    INTEGER :: A
+    REAL    :: B, RESULT
     INCLUDE '../../chdir_to_code.inc'
 
-    PRINT *, 'Enter 2 numbers:'
-    READ(*, *) A, B
+    PRINT *, 'Enter an integer:'
+    READ(*, *) A
+
+    PRINT *, 'Enter a real number:'
+    READ(*, *) B
+
+    RESULT = REAL(A) + B
+
+    PRINT '(A, F10.2)', 'The sum is: ', RESULT
 
     OPEN(UNIT=10, FILE='ex1_results.txt', STATUS='REPLACE', ACTION='WRITE')
-    WRITE(10, '(A, I0, A, I0)') 'The integer numbers are: ', A, ', ', B
+    WRITE(10, '(A, F10.2)') 'The sum is: ', RESULT
     CLOSE(10)
 
 END PROGRAM EX1
