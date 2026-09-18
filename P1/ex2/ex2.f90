4929825df093ed27967200b5854e47fd5bcffef7
HP_ARMONBAI\arnau <arnau.montull@gmail.com>
2026-09-18 12:03:51 +0200

    Refactor EX2 program to improve precision in pi approximation, update variable types to DOUBLE PRECISION, and streamline output writing process.

--- P1/ex2/ex2.f90
+++ P1/ex2/ex2.f90
@@ -1,23 +1,21 @@
 PROGRAM EX2
     IMPLICIT NONE
-    REAL, PARAMETER :: pi = 3.141592653589
-    REAL :: pi_aprox = 0.0000000, error = 1.0000000
-    INTEGER :: K = 0
-    INTEGER :: I
-    REAL, DIMENSION(294337,2) :: results
+    DOUBLE PRECISION, PARAMETER :: pi = 3.14159265358979323846D0
+    DOUBLE PRECISION ::  pi_aprox = 0.0D0, error    = 1.0D0
+    INTEGER ::K = 0, sign_K   = 1
     INCLUDE '../../chdir_to_code.inc'
-
-    DO WHILE (abs(error) > 0.001)
-        pi_aprox = pi_aprox + (-1)**K * 4.0 / (2.0*K + 1)
-        error = pi_aprox - pi
-        K = K + 1
-        results(K,1) = K
-        results(K,2) = pi_aprox
-    END DO
     OPEN(UNIT=10, FILE='ex2_results.dat', STATUS='REPLACE', ACTION='WRITE')
-    DO I = 1, K
-        WRITE(10, '(I8, 1X, ES16.8)') NINT(results(I,1)), results(I,2)
+
+    DO WHILE (ABS(error) > 1.0D-6)
+        pi_aprox = pi_aprox + sign_K * 4.0D0 / (2.0D0 * DBLE(K) + 1.0D0)
+        error = pi_aprox - pi
+        
+        K = K + 1
+        WRITE(10, '(I8, 1X, ES16.8)') K, pi_aprox
+        sign_K = -sign_K
     END DO
+
     CLOSE(10)
-    PRINT *, 'The approximate value of pi is for K =', K, 'is: ', pi_aprox
+
+    PRINT *, 'The approximate value of pi for K =', K, 'is: ', pi_aprox
 END PROGRAM EX2
