392ee704061b47b9b58c62e495c233ba78bdabe4
HP_ARMONBAI\arnau <arnau.montull@gmail.com>
2026-09-18 13:56:55 +0200

    Refactor EX5 program to replace INT with NINT for time step calculations in output loops and update subroutine signatures to use allocatable arrays for improved memory management.

--- P1/ex5/ex5.f90
+++ P1/ex5/ex5.f90
@@ -14,7 +14,7 @@ PROGRAM EX5
     CALL EULER(m,k,t_max,dt,x_0,v_0,t_0,x_euler_001,v_euler_001,t_euler_001)
     OPEN(10,file='euler_001.dat')
     WRITE(10,*) 't,x,v'
-    DO i = 1, INT(t_max/dt)+1
+    DO i = 1, NINT(t_max/dt)+1
         WRITE(10,*) t_euler_001(i),x_euler_001(i),v_euler_001(i)
     END DO
     CLOSE(10)
@@ -23,7 +23,7 @@ PROGRAM EX5
     CALL EULER(m,k,t_max,dt,x_0,v_0,t_0,x_euler_01,v_euler_01,t_euler_01)
     OPEN(11,file='euler_01.dat')
     WRITE(11,*) 't,x,v'
-    DO i = 1, INT(t_max/dt)+1
+    DO i = 1, NINT(t_max/dt)+1
         WRITE(11,*) t_euler_01(i),x_euler_01(i),v_euler_01(i)
     END DO
     CLOSE(11)
@@ -32,7 +32,7 @@ PROGRAM EX5
     CALL EULER(m,k,t_max,dt,x_0,v_0,t_0,x_euler_02,v_euler_02,t_euler_02)
     OPEN(12,file='euler_02.dat')
     WRITE(12,*) 't,x,v'
-    DO i = 1, INT(t_max/dt)+1
+    DO i = 1, NINT(t_max/dt)+1
         WRITE(12,*) t_euler_02(i),x_euler_02(i),v_euler_02(i)
     END DO
     CLOSE(12)
@@ -40,7 +40,7 @@ PROGRAM EX5
     CALL EULER_PREDICTOR(m,k,t_max,dt,x_0,v_0,t_0,x_euler_predictor,v_euler_predictor,t_euler_predictor)
     OPEN(13,file='euler_predictor.dat')
     WRITE(13,*) 't,x,v'
-    DO i = 1, INT(t_max/dt)+1
+    DO i = 1, NINT(t_max/dt)+1
         WRITE(13,*) t_euler_predictor(i),x_euler_predictor(i),v_euler_predictor(i)
     END DO
     CLOSE(13)
@@ -48,7 +48,7 @@ PROGRAM EX5
     CALL VERLET(m,k,t_max,dt,x_0,v_0,t_0,x_verlet,v_verlet,t_verlet)
     OPEN(14,file='verlet.dat')
     WRITE(14,*) 't,x,v'
-    DO i = 1, INT(t_max/dt)+1
+    DO i = 1, NINT(t_max/dt)+1
         WRITE(14,*) t_verlet(i),x_verlet(i),v_verlet(i)
     END DO
     CLOSE(14)
@@ -57,15 +57,18 @@ CONTAINS
     SUBROUTINE EULER(m,k,t_max,dt,x_0,v_0,t_0,x,v,t)
         IMPLICIT NONE
         REAL, INTENT(IN) :: m,k,t_max,dt,x_0,v_0,t_0
-        REAL, DIMENSION(:), INTENT(OUT) :: x,v,t
+        REAL, ALLOCATABLE, DIMENSION(:), INTENT(OUT) :: x,v,t
         REAL :: a
-        INTEGER :: i
+        INTEGER :: i,n
+        
+        n = NINT((t_max - t_0) / dt) + 1
+        ALLOCATE(x(n), v(n), t(n))
 
         x(1)=x_0
         v(1)=v_0
         t(1)=t_0
-
-        DO i = 2, INT(t_max/dt)+1
+       
+        DO i = 2, n
             a=-(k/m)*x(i-1)
             v(i)=v(i-1)+a*dt
             x(i)=x(i-1)+v(i-1)*dt+0.5*a*dt**2
@@ -76,11 +79,12 @@ CONTAINS
     SUBROUTINE VERLET(m, k, t_max, dt, x_0, v_0, t_0, x, v, t)
         IMPLICIT NONE
         REAL, INTENT(IN) :: m, k, t_max, dt, x_0, v_0, t_0
-        REAL, DIMENSION(:), INTENT(OUT) :: x, v, t
+        REAL, ALLOCATABLE, DIMENSION(:), INTENT(OUT) :: x, v, t
         REAL :: a
         INTEGER :: i, n
     
         n = NINT((t_max - t_0) / dt) + 1
+        ALLOCATE(x(n), v(n), t(n))
     
         t(1) = t_0
         x(1) = x_0
@@ -106,15 +110,18 @@ CONTAINS
     SUBROUTINE EULER_PREDICTOR(m,k,t_max,dt,x_0,v_0,t_0,x,v,t)
         IMPLICIT NONE
         REAL, INTENT(IN) :: m,k,t_max,dt,x_0,v_0,t_0
-        REAL, DIMENSION(:), INTENT(OUT) :: x,v,t
+        REAL, ALLOCATABLE, DIMENSION(:), INTENT(OUT) :: x,v,t
         REAL :: a, x_p, a_p
-        INTEGER :: i
+        INTEGER :: i,n
+
+        n = NINT((t_max - t_0) / dt) + 1
+        ALLOCATE(x(n), v(n), t(n))
 
         x(1)=x_0
         v(1)=v_0
         t(1)=t_0
 
-        DO i = 2, NINT(t_max/dt)+1
+        DO i = 2, n
             a=-(k/m)*x(i-1)
             x_p=x(i-1)+v(i-1)*dt+0.5*a*dt**2
             a_p=-(k/m)*x_p
